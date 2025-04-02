63a3c9c3b6d4631d5d04c8d7c3627d2ef1573383
Jaiden Troenodikromo <6016497@mborijnland.nl>
2025-04-02 13:49:06 +0200

    style changes

--- prescription.php
+++ prescription.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Apothecare - Upload Prescription</title>
+    <title>phart - Upload Prescription</title>
     <link rel="stylesheet" href="styles.css">
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -13,8 +13,8 @@
 <body>
     <header>
         <div class="logo">
-            <i class="fas fa-bolt"></i>
-            <h1>Apothecare</h1>
+        <i class='fab fa-medrt' id="icon"></i>
+            <h1>phart</h1>
         </div>
         <nav>
             <ul>
