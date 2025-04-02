462e8d02c9b5dc60a7f6c0b097552ee54c3c95f1
Tijn van Elburg <6046216@mborijnland.nl>
2025-04-02 14:36:17 +0200

    configuration changes

--- register.php
+++ register.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Apothecare - Create Account</title>
-    <link rel="stylesheet" href="CSS/login.css">
+    <link rel="stylesheet" href="css/register.css">
 </head>
 <body>
     <header>
