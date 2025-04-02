b879ba331adb3191ad51046cd5699f3174f47b51
Tijn van Elburg <6046216@mborijnland.nl>
2025-04-02 09:51:29 +0200

    linken van style en script.

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Homepage</title>
 </head>
 <body>
+    <h1>Dit is een test</h1>
     <script src="js/script.js"></script>
 </body>
 </html>
