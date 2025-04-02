af7ca9f97b86d42f7c56a296bd0a1f8ef433bd1e
Tijn van Elburg <6046216@mborijnland.nl>
2025-04-02 11:10:28 +0200

    database connect

--- config.php
+++ config.php
@@ -1,2 +1,16 @@
 <?php
 
+// config.php - Databaseverbinding
+$host = 'localhost';
+$port = '3306';
+$dbname = 'phart';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Databaseverbinding mislukt: " . $e->getMessage());
+}
+?>
